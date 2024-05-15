2d1b134d9941c2c551938b08a70a14fb1e70e5f5
stalogic <jiangmings1992@163.com>
2024-05-15 15:03:10 +0800

    完成初步重构
    1. 对环境进行包装
    2. 将奖励函数，状态函数，info包装进新的env中

---
+++ experiment/envs.py
@@ -0,0 +1,106 @@
+import gym
+
+class MountainCarEnv():
+
+    def __init__(self) -> None:
+        self.env = gym.make('MountainCar-v0')
+        self.state_dim = self.env.observation_space.shape[0]
+        self.action_dim = self.env.action_space.n
+
+        # infomation
+        self.rightmost_position = None
+        self.max_speed = None
+        self.total_reward = 0
+        self.total_raw_reward = 0
+        self.total_steps = 0
+
+    def reset(self):
+        obs, _ = self.env.reset()
+        self.rightmost_position = obs[0]
+        self.max_speed = abs(obs[1])
+        
+        if hasattr(self, 'state_func'):
+            obs = self.state_func(obs)
+
+        info = {
+            'rightmost_position': self.rightmost_position,
+            'max_speed': self.max_speed,
+            'total_reward': self.total_reward,
+            'total_raw_reward': self.total_raw_reward,
+            'total_steps': self.total_steps
+        }
+        return obs, info
+    
+    def sample_action(self):
+        return self.env.action_space.sample()
+    
+    def step(self, action):
+        obs, reward, done, terminal, _ = self.env.step(action)
+
+        self.total_steps += 1
+        self.rightmost_position = max(self.rightmost_position, obs[0])
+        self.max_speed = max(self.max_speed, abs(obs[1]))
+        self.total_raw_reward += reward
+        if hasattr(self, 'state_func'):
+            obs = self.state_func(obs)
+        if hasattr(self, 'reward_func'):
+            reward = self.reward_func(obs, reward)
+        self.total_reward += reward
+        info = {
+            'rightmost_position': self.rightmost_position,
+            'max_speed': self.max_speed,
+            'total_reward': self.total_reward,
+            'total_raw_reward': self.total_raw_reward,
+            'total_steps': self.total_steps
+        }
+
+        return obs, reward, done, terminal, info
+    
+
+def mountain_car_v0():
+    env = MountainCarEnv()
+    return env
+
+def mountain_car_v0_reward_redefined():
+    def _reward(self, obs, reward):
+        x, v = obs
+        if x >= 0.5:
+            return 100
+        return reward + 10 * (abs(x+0.5) + 10 * abs(v))
+    
+    env = MountainCarEnv()
+    setattr(MountainCarEnv, "reward_func", _reward)
+    return env
+
+def mountain_car_v0_state_reward_redefined():
+    import numpy as np
+    def _state(self, obs):
+        position, velocity = obs
+        self.steps_remaining -= 1
+        return np.array([position, velocity, self.steps_remaining / 200])
+    
+    def _reward(self, obs, reward):
+        x, v, s = obs
+        if x >= 0.5:
+            return 100 + 10 * s
+        return reward + 10 * (abs(x+0.5) + 10 * abs(v))
+    
+    env = MountainCarEnv()
+    env.state_dim += 1
+    setattr(env, "steps_remaining", 201)
+    setattr(MountainCarEnv, "state_func", _state)
+    setattr(MountainCarEnv, "reward_func", _reward)
+    return env
+
+
+if __name__ == '__main__':
+    env = mountain_car_v0_state_reward_redefined()
+    obs, info = env.reset()
+    print(obs, info)
+
+    while True:
+        action = env.sample_action()
+        obs, reward, done, terminal, info = env.step(action)
+        print(obs, reward, done, terminal, info)
+        if done or terminal:
+            break
--- experiment/mountaincar-v0/dqn_mountaincar_v0.py
+++ experiment/mountaincar-v0/dqn_mountaincar_v0.py
@@ -1,26 +1,17 @@
-import gym
 import numpy as np
 import tqdm
 import os
-print(os.getcwd())
+print(f"running in {os.getcwd()}")
 os.sys.path.append(os.getcwd())
-from algorithm.dqn import DQN
+from algorithm import DQN
+from experiment.envs import mountain_car_v0_reward_redefined
 
-def adjust(next_state):
-    position, velocity = next_state
-    if position > 0.5:
-        return 100
-    button_x = -0.5
-    distance = abs(position - button_x)
-    return (distance + 10*abs(velocity)) * 10 - 1
-
-env = gym.make('MountainCar-v0')
-state_dim = env.observation_space.shape[0]
-action_dim = env.action_space.n
+env = mountain_car_v0_reward_redefined()
+state_dim = env.state_dim
+action_dim = env.action_dim
 hidden_dim = 128
 batch_size = 128
-num_episodes = 1000
-
+num_episodes = 10000
 
 agent = DQN(state_dim, action_dim, hidden_dim, batch_size)
 
@@ -29,37 +20,31 @@ metrics = {
     "raw_reward": [],
     "done": [],
     "terminal": [],
-    "v_loss": [],
-    "p_loss": [],
     "max_pos": [],
     "max_speed": [],
+    "max_steps": []
 }
 
 with tqdm.tqdm(range(num_episodes)) as pbar:
     for episode in pbar:
         state, _ = env.reset()
-        total_reward = 0
-        total_raw_reward = 0
-        max_pos = -1
-        max_speed = -1
+        
         while True:
             action = agent.take_action(state)
-            next_state, reward, done, terminal, _ = env.step(action)
-            total_raw_reward += reward
-            reward = adjust(next_state)
-            total_reward += reward
-            max_pos = max(max_pos, next_state[0]) if max_pos else next_state[0]
-            max_speed = max(max_speed, next_state[1]) if max_speed else next_state[1]
+            next_state, reward, done, terminal, info = env.step(action)
             agent.add_experience(state, action, reward, next_state, done)
             state = next_state
 
             if done or terminal:
                 metrics["done"].append(1 if done else 0)
                 metrics["terminal"].append(1 if terminal else 0)
-                metrics["reward"].append(total_reward)
-                metrics["raw_reward"].append(total_raw_reward)
-                metrics["max_pos"].append(max_pos)
-                metrics["max_speed"].append(max_speed)
+                
+                total_steps = info.get("total_steps")
+                metrics["max_steps"].append(total_steps)
+                metrics["reward"].append(info.get("total_reward") / total_steps)
+                metrics["raw_reward"].append(info.get("total_raw_reward") / total_steps)
+                metrics["max_pos"].append(info.get("rightmost_position"))
+                metrics["max_speed"].append(info.get("max_speed"))
                 break
         
         agent.update()
@@ -69,6 +54,7 @@ with tqdm.tqdm(range(num_episodes)) as pbar:
                 "Episode": episode,
                 "Reward": np.mean(metrics["reward"]),
                 "Raw Reward": np.mean(metrics["raw_reward"]),
+                "Max Steps": np.mean(metrics["max_steps"]),
                 "Done": np.mean(metrics["done"]),
                 "Terminal": np.mean(metrics["terminal"]),
                 "Max Pos": np.max(metrics["max_pos"]),
