3f6114b83b8e013846b77403841ca20364068245
stalogic <jiangmings1992@163.com>
2024-09-27 15:34:07 +0800

    Update build_venv.sh

--- build_venv.sh
+++ build_venv.sh
@@ -1,8 +1,16 @@
-conda create -n xxx python=3.10
+conda create -n rlpark python=3.10
+conda activate rlpark
 
-# pip3 install torch torchvision torchaudio
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
-pip install 
+pip install numpy==1.26
+
+pip install torch torchvision torchaudio
+# pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
 pip install typer
-pip install gymnasium
-pip install "gym[atari, accept-rom-license]"
+pip install wandb
+pip install tqdm
+pip install black
+pip install opencv-python
+# pip install "gym[classic_control]"
+# pip install "gym[atari, accept-rom-license]"
+pip install "gymnasium[classic_control]"
+pip install "gymnasium[atari, accept-rom-license]"
