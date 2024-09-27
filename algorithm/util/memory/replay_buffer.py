f4cdd1db6b945e1ecb39b410e56f8943e1f6a358
stalogic <jiangmings1992@163.com>
2024-09-27 10:53:26 +0800

    新增 Prioritized Replay buffer，并合并 replay buffer的目录结构

--- algorithm/util/memory/replay_buffer.py
+++ algorithm/util/memory/replay_buffer.py
@@ -2,12 +2,13 @@ import numpy as np
 import collections
 import random
 
+
 class ReplayBuffer(object):
     def __init__(self, capacity=100000, *args, **kwargs):
         self.buffer = collections.deque(maxlen=capacity)
 
     def __len__(self):
-         return len(self.buffer)
+        return len(self.buffer)
 
     def store(self, state, action, reward, next_state, done):
         self.buffer.append((state, action, reward, next_state, done))
