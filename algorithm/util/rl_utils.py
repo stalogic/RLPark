0fad0c4145a6d37ade37d22c3a98feb18bffd243
stalogic <jiangmings1992@163.com>
2024-09-27 11:02:08 +0800

    新增设置随机数种子函数，取消on-policy 训练过程种进度条上显示信息

--- algorithm/util/rl_utils.py
+++ algorithm/util/rl_utils.py
@@ -1,10 +1,21 @@
+import os
 import time
-import torch
 import tqdm
+import torch
+import random
 import numpy as np
 import wandb
 
 from .base_rl_model import OnPolicyRLModel, OffPolicyRLModel
+
+
+def set_seed(env, seed=0):
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    env.action_space.seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+
 
 class MovingAverage(object):
     def __init__(self, gamma=0.9) -> None:
@@ -22,6 +33,7 @@ class MovingAverage(object):
     def value(self) -> int:
         return int(self.sum / self.factor)
 
+
 def compute_advantage(gamma, lamda, td_delta):
     td_delta = td_delta.detach().cpu().numpy()
     advantage_list = []
@@ -32,10 +44,11 @@ def compute_advantage(gamma, lamda, td_delta):
     advantage_list.reverse()
     return torch.tensor(np.array(advantage_list), dtype=torch.float)
 
+
 def train_and_evaluate(env, agent, num_episodes=1000, val_env=None, **kwargs):
     """
     训练和评估一个智能体在一个给定环境中的表现。
-    
+
     参数:
     - env: 环境接口，用于模拟智能体所处的环境。
     - agent: 智能体接口，用于执行动作和学习经验。
@@ -44,22 +57,25 @@ def train_and_evaluate(env, agent, num_episodes=1000, val_env=None, **kwargs):
         - num_eval_frequence: 评估频率，即每隔多少个episode进行一次性能评估，默认若未提供则为100。
         - num_eval_episodes: 在每次评估周期中执行的评估episode数量，默认若未提供则为10。
         其他自定义配置项也可通过此方式传递，但需确保在函数内部有相应的处理逻辑。
-    
+
     返回值:
     - 无
     """
-    
+
     if hasattr(agent, "replay_buffer"):
         train_and_evaluate_offpolicy_agent(env, agent, num_episodes, val_env, **kwargs)
     else:
         train_and_evaluate_onpolicy_agent(env, agent, num_episodes, val_env, **kwargs)
 
-def train_and_evaluate_offpolicy_agent(env, agent:OffPolicyRLModel, num_episodes=1000, val_env=None, **kwargs):
+
+def train_and_evaluate_offpolicy_agent(
+    env, agent: OffPolicyRLModel, num_episodes=1000, val_env=None, **kwargs
+):
 
     # 统计单局游戏的移动平均步数，即episode长度的移动平均
     game_step = MovingAverage()
     train_freq = None
-    
+
     # 初始化用于记录指标的字典
     metrics = {
         "episode_return": [],  # 每个episode的回报
@@ -74,7 +90,12 @@ def train_and_evaluate_offpolicy_agent(env, agent:OffPolicyRLModel, num_episodes
             # 重置环境，开始新episode
             state, _ = env.reset()
             game_time, update_time = 0.0, 0.0
-            episode_return, episode_step, episode_done, episode_terminal = 0.0, 0, None, None
+            episode_return, episode_step, episode_done, episode_terminal = (
+                0.0,
+                0,
+                None,
+                None,
+            )
             while True:
                 t0 = time.time()
                 # 选择并执行动作
@@ -98,13 +119,13 @@ def train_and_evaluate_offpolicy_agent(env, agent:OffPolicyRLModel, num_episodes
                     agent.update()
 
                 update_time += time.time() - t0
-                
+
                 # 判断episode是否结束
                 if done or terminal:
                     episode_done = int(done)
                     episode_terminal = int(terminal)
                     break
-            
+
             # 更新学习率
             agent.update_learning_rate()
 
@@ -120,16 +141,22 @@ def train_and_evaluate_offpolicy_agent(env, agent:OffPolicyRLModel, num_episodes
                 "TR_Episode_Done": episode_done,
                 "TR_Episode_Terminal": episode_terminal,
                 "TR_Game_Time": game_time,
-                "TR_Update_Time":update_time - game_time,
+                "TR_Update_Time": update_time - game_time,
             }
 
-            try: wandb.log(logdata)
-            except: pass
+            try:
+                wandb.log(logdata)
+            except:
+                pass
 
             # 达到评估周期时进行性能评估
             if (i_episode + 1) % kwargs.get("num_eval_frequence", 100) == 0:
                 # 执行多轮评估episode
-                for i_val in tqdm.tqdm(range(kwargs.get("num_eval_episodes", 100)), desc="Evaluating", leave=False):
+                for i_val in tqdm.tqdm(
+                    range(kwargs.get("num_eval_episodes", 100)),
+                    desc="Evaluating",
+                    leave=False,
+                ):
                     episode_return, episode_step = 0.0, 0
 
                     if i_val % kwargs.get("render_period", 1) == 0:
@@ -140,16 +167,18 @@ def train_and_evaluate_offpolicy_agent(env, agent:OffPolicyRLModel, num_episodes
                     while True:
                         # 使用预测模式选择动作
                         if hasattr(env_, "action_mask"):
-                            action = agent.predict_action_with_mask(state, env_.action_mask)
+                            action = agent.predict_action_with_mask(
+                                state, env_.action_mask
+                            )
                         else:
                             action = agent.predict_action(state)
                         next_state, reward, done, terminal, _ = env_.step(action)
                         state = next_state
-                        
+
                         # 累加评估指标
                         episode_return += reward
                         episode_step += 1
-                        
+
                         # 记录评估结果
                         if done or terminal:
                             metrics["episode_return"].append(episode_return)
@@ -166,18 +195,22 @@ def train_and_evaluate_offpolicy_agent(env, agent:OffPolicyRLModel, num_episodes
                     "Ev_Avg_Done": np.mean(metrics["episode_done"]),
                     "Ev_Avg_Terminal": np.mean(metrics["episode_terminal"]),
                 }
-                
+
                 # 清空指标准备下一轮评估
                 for key in metrics.keys():
                     metrics[key] = []
 
                 # 更新进度条信息，并尝试通过wandb记录日志
                 pbar.set_postfix(logdata)
-                try: wandb.log(logdata)
-                except: pass  # 忽略wandb未安装或配置不当的情况
+                try:
+                    wandb.log(logdata)
+                except:
+                    pass  # 忽略wandb未安装或配置不当的情况
 
 
-def train_and_evaluate_onpolicy_agent(env, agent:OnPolicyRLModel, num_episodes=1000, val_env=None, **kwargs):
+def train_and_evaluate_onpolicy_agent(
+    env, agent: OnPolicyRLModel, num_episodes=1000, val_env=None, **kwargs
+):
 
     # 初始化用于记录指标的字典
     metrics = {
@@ -192,8 +225,19 @@ def train_and_evaluate_onpolicy_agent(env, agent:OnPolicyRLModel, num_episodes=1
         for i_episode in pbar:
             # 重置环境，开始新episode
             state, _ = env.reset()
-            transition_dict = {'states': [], 'actions': [], 'next_states': [], 'rewards': [], 'dones': []}
-            episode_return, episode_step, episode_done, episode_terminal = 0.0, 0, None, None
+            transition_dict = {
+                "states": [],
+                "actions": [],
+                "next_states": [],
+                "rewards": [],
+                "dones": [],
+            }
+            episode_return, episode_step, episode_done, episode_terminal = (
+                0.0,
+                0,
+                None,
+                None,
+            )
             t0 = time.time()
             while True:
                 # 选择并执行动作
@@ -204,13 +248,13 @@ def train_and_evaluate_onpolicy_agent(env, agent:OnPolicyRLModel, num_episodes=1
                 # 环境反馈，更新状态
                 next_state, reward, done, terminal, _ = env.step(action)
                 # 学习经验
-                transition_dict['states'].append(state)
-                transition_dict['actions'].append(action)
-                transition_dict['next_states'].append(next_state)
-                transition_dict['rewards'].append(reward)
-                transition_dict['dones'].append(done)
+                transition_dict["states"].append(state)
+                transition_dict["actions"].append(action)
+                transition_dict["next_states"].append(next_state)
+                transition_dict["rewards"].append(reward)
+                transition_dict["dones"].append(done)
                 state = next_state
-                
+
                 episode_return += reward
                 episode_step += 1
 
@@ -221,7 +265,7 @@ def train_and_evaluate_onpolicy_agent(env, agent:OnPolicyRLModel, num_episodes=1
                     break
 
             t1 = time.time()
-            
+
             # 更新智能体模型
             agent.update(transition_dict)
             agent.update_learning_rate()
@@ -238,14 +282,20 @@ def train_and_evaluate_onpolicy_agent(env, agent:OnPolicyRLModel, num_episodes=1
             }
 
             # 更新进度条信息，并尝试通过wandb记录日志
-            pbar.set_postfix(logdata)
-            try: wandb.log(logdata)
-            except: pass  # 忽略wandb未安装或配置不当的情况
+            # pbar.set_postfix(logdata)
+            try:
+                wandb.log(logdata)
+            except:
+                pass  # 忽略wandb未安装或配置不当的情况
 
             # 达到评估周期时进行性能评估
             if (i_episode + 1) % kwargs.get("num_eval_frequence", 100) == 0:
                 # 执行多轮评估episode
-                for i_val in tqdm.tqdm(range(kwargs.get("num_eval_episodes", 100)), desc="Evaluating", leave=False):
+                for i_val in tqdm.tqdm(
+                    range(kwargs.get("num_eval_episodes", 100)),
+                    desc="Evaluating",
+                    leave=False,
+                ):
                     episode_return, episode_step = 0.0, 0
 
                     if i_val % kwargs.get("render_period", 1) == 0:
@@ -256,16 +306,18 @@ def train_and_evaluate_onpolicy_agent(env, agent:OnPolicyRLModel, num_episodes=1
                     while True:
                         # 使用预测模式选择动作
                         if hasattr(env_, "action_mask"):
-                            action = agent.predict_action_with_mask(state, env_.action_mask)
+                            action = agent.predict_action_with_mask(
+                                state, env_.action_mask
+                            )
                         else:
                             action = agent.predict_action(state)
                         next_state, reward, done, terminal, _ = env_.step(action)
                         state = next_state
-                        
+
                         # 累加评估指标
                         episode_return += reward
                         episode_step += 1
-                        
+
                         # 记录评估结果
                         if done or terminal:
                             metrics["episode_return"].append(episode_return)
@@ -282,12 +334,14 @@ def train_and_evaluate_onpolicy_agent(env, agent:OnPolicyRLModel, num_episodes=1
                     "Ev_Avg_Done": np.mean(metrics["episode_done"]),
                     "Ev_Avg_Terminal": np.mean(metrics["episode_terminal"]),
                 }
-                
+
                 # 清空指标准备下一轮评估
                 for key in metrics.keys():
                     metrics[key] = []
 
                 # 更新进度条信息，并尝试通过wandb记录日志
                 pbar.set_postfix(logdata)
-                try: wandb.log(logdata)
-                except: pass  # 忽略wandb未安装或配置不当的情况
+                try:
+                    wandb.log(logdata)
+                except:
+                    pass  # 忽略wandb未安装或配置不当的情况
